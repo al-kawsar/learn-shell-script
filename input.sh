1ce1537482fd5694838720c3a5c307c153771b1a
andirhn <tlod9906@gmail.com>
2024-03-07 21:14:06 -1200

    #3 Read User Input

---
+++ input.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# Belajar Input Shell
+
+echo -n "username : "
+read username
+read -sp 'password : ' password
+
+echo -e '\n'
+
+echo "your username : $username"
+echo "your password : $password"
